879c58e69c97416ae49915047e53247ef199820f
Dicara Francesco <dicaraf@gmail.com>
2024-10-25 06:55:21 +0000

    numeri random

--- TerzaI/Informatica/EserciziSelezioneC/22102024_calcolatrice_switch.c
+++ TerzaI/Informatica/EserciziSelezioneC/22102024_calcolatrice_switch.c
@@ -3,22 +3,34 @@ int main(){
     int num1=0;
     int num2=0;
     char operazione;
+    float risultato;
+    int flag=0;
 
     printf(" inserisci il primo numero: ");
     scanf("%d", &num1);
     printf("inserisci il secondo numero: ");
     scanf("%d", &num2);
     printf("quale operazione vuoi fare:");
-    scanf("%c", &operazione);
+    scanf(" %c", &operazione);
     switch(operazione){
-        case 'a': 
-
+        case 'a':
+                    risultato=num1+num2;
+                    break;
         case 'm': 
+                    risultato=num1*num2;
+                    break;
         case 's':
+                    risultato=num1-num2;
+                    break;
         case 'd':
-
-        printf("")
+                    risultato=(float)num1/(float)num2;
+                    break;
+        default: 
+                    flag=1;
+                    printf("l'operazione scelta non è disponibile");
     }
+    if(flag==0)
+                    printf("il risultato della operazione è: %f", risultato);
     
     
     
---
+++ TerzaI/Informatica/EserciziSelezioneC/25102024_random.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    srand(time(NULL));
+
+    int random_max=31;
+    int random_min=1;
+
+    int random = rand()%(random_max - random_min + 1) + random_min;
+
+    printf("numero random: %d", random);
+
+
+
+
+}
