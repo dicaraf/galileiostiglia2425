0f987bc6e7c25dc4bea74dcac5f409aab4b0825a
Dicara Francesco <dicaraf@gmail.com>
2024-11-06 07:54:53 +0000

    esercizio ripasso per verifica

--- TerzaI/Informatica/EserciziSelezioneC/05112024_ripasso.c
+++ TerzaI/Informatica/EserciziSelezioneC/05112024_ripasso.c
@@ -15,18 +15,50 @@ int main(){
     float voto1;
     float voto2;
     int scelta;
-    printf("Inserisci il primo voto");
+    printf("Inserisci il primo voto: ");
     scanf("%f", &voto1);
     srand(time(NULL));
     voto2 = rand()%5+4;
-    printf("Il voto casuale è %f", voto2);
+    printf("Il voto casuale è: %.0f", voto2);
     printf("Scegli un'operazione \n- 1 mostrare la media dei due voti \n- 2 mostrare il voto maggiore tra i due\n- 3 mostrare il voto minore tra i due. \n- 4 mostrare il numero di voti sufficiente tra i due.");
     scanf("%d", &scelta);
-    
-    switch(scelta){
-        case 1:
-            
+    if(voto1>=2 && voto1<=10){
+        switch(scelta){
+            case 1:
+                printf("la media dei due numeri è: %.2f\n", (voto1+voto2)/2);
+                break;
+            case 2:
+                if(voto1>voto2){
+                    printf("il primo voto è maggiore.\n");
+                }else if(voto1==voto2){
+                    printf("i voti sono uguali.\n");
+                }else{
+                    printf("il secondo voto è maggiore.\n");
+                }
+                break;
+            case 3:
+                if(voto1<voto2){
+                    printf("il primo voto è minore.\n");
+                }else if(voto1==voto2){
+                    printf("i voti sono uguali.\n");
+                }else{
+                    printf("il secondo voto è minore.\n");
+                }
+                break;
+            case 4:
+                if(voto1>=6 && voto2>=6){
+                    printf("entrambi i voti sono sufficienti.\n");
+                }else if(voto1>=6 || voto2>=6)
+                    printf("un solo voto è sufficiente.\n");
+                else
+                    printf("entrambi i voti sono insufficienti.\n");
+                break;
+            default:
+                printf("è stata scelta un opzione non valida.\n");
+        }
+    }else
+        printf("il voto inserito non è valido.\n");
 
-    }
+    
 
 }
--- TerzaI/Informatica/EserciziSelezioneC/25102024_random.c
+++ TerzaI/Informatica/EserciziSelezioneC/25102024_random.c
@@ -12,7 +12,7 @@ int main(){
 
     printf("numero random: %d", random);
 
-
+    return 0;
 
 
 }
