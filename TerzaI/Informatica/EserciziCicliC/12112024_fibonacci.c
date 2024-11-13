eed83605515950936077eff4d0823ebc407cc82b
Dicara Francesco <dicaraf@gmail.com>
2024-11-13 09:33:28 +0000

    esempi cicli while

--- TerzaI/Informatica/EserciziCicliC/12112024_fibonacci.c
+++ TerzaI/Informatica/EserciziCicliC/12112024_fibonacci.c
@@ -1,3 +1,27 @@
 /*scrivi un programma che chieda all'utente un numero che sia necessariamente maggiore di 3 e che indica quanti valori della sequenza di fibonacci mostrare.
-Per esempio se l'utente inserisce 6 dovranno essere mostrati 1 1 2 3 5 8.
-Vedi pag 160*/
+Per esempio se l'utente inserisce 6 dovranno essere mostrati 1 1 2 3 5 8.*/
+#include<stdio.h>
+int main()
+{
+    long long num=0,num1=1,num2=1,num3=0,contatore=2;
+    printf("Inserisci un valore maggiore di 3:\t");
+    scanf("%lld", &num);
+    while (num<3)
+    {
+        printf("Inserisci un valore maggiore di 3:\t");
+        scanf("%lld", &num);
+    }
+    printf("%lld %lld",num1,num2);
+    while (num!=contatore)
+    {
+        num3=num1+num2;
+        printf(" %lld",num3);
+        num1=num2;
+        num2=num3;
+        contatore++;
+    }
+    
+    
+        
+    
+}
---
+++ TerzaI/Informatica/EserciziCicliC/13112024_indovina.c
@@ -0,0 +1,23 @@
+/*Genera un numero casuale compreso tra 10 e 1000 e chiedi all'utente di indovinare il numero finchè non azzecca. Per ogni numero inserito dai un suggerimento all'utente specificando se il tentativo è minore o maggiore del numero da indovinare.*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    srand(time(NULL));
+    int numcasuale=0,num=0;
+    numcasuale=random()%991+10;
+
+    while(numcasuale!=num){
+        printf("\nprova ad indovinare il numero: ");
+        scanf("%d", &num);
+
+        if(numcasuale<num)
+            printf("\nil numero inserito è maggiore del numero da indovinare");
+        else if(numcasuale>num)
+            printf("\nil numero inserito è minore del numero da indovinare");
+    }
+    printf("\nhai indovinato il numero!");
+        
+
+}
