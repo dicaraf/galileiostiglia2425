84add85ac97fc4643264daaadf515b0aa33af6da
Dicara Francesco <dicaraf@gmail.com>
2025-02-05 08:15:12 +0000

    banca completo

--- TerzaI/Informatica/Funzioni/esercizio_completo.c
+++ TerzaI/Informatica/Funzioni/esercizio_completo.c
@@ -8,6 +8,7 @@ Funzionalità:
 
  #include <stdio.h>
  #include <stdlib.h>
+ #include <unistd.h>
 
  // Funzione per depositare denaro
 void deposita(float *_saldo, float _importo);
@@ -22,8 +23,9 @@ void mostra_saldo(float _saldo, int _decimali);
 void mostra_menu();
 
 int main(){
-    float saldo, importo;
+    float saldo=0, importo;
     int scelta = -1;
+    int dec;
     while(scelta != 0) {
         mostra_menu();
         printf("Scegli quale operazione vuoi fare: ");
@@ -32,15 +34,17 @@ int main(){
             case 1:
                 printf("Quale cifra vuoi depositare? ");
                 scanf("%f", &importo);
-                
+                deposita(&saldo, importo);
                 break;
             case 2:
                 printf("Quale cifra vuoi prelevare? ");
                 scanf("%f", &importo);
-                
+                preleva(&saldo, importo);
                 break;
             case 3:
-
+                printf("Quanti decimali vuoi mostrare nel tuo saldo? ");
+                scanf("%d", &dec);
+                mostra_saldo(saldo, dec);
                 break;
             case 4:
                 printf("Il programma si sta per chiudere....");
@@ -62,7 +66,8 @@ void preleva(float *_saldo, float _importo){
     if(_importo<=*_saldo){
         *_saldo-=_importo;
     }else
-    printf("Non hai abbastanza denaro\n: ");
+        printf("Non hai abbastanza denaro\n: ");
+        
 }
 void mostra_saldo(float _saldo, int _decimali){
     switch (_decimali)
@@ -70,17 +75,16 @@ void mostra_saldo(float _saldo, int _decimali){
     case 0:
         printf("Il tuo saldo è %.0f\n", _saldo);
         break;
-    
     case 1:
         printf("Il tuo saldo è %.1f\n", _saldo);
         break;
-    
     default:
         printf("Il tuo saldo è %.2f\n", _saldo);
         break;
     }
 }
 void mostra_menu(){
+    sleep(5);
     system("clear");
     printf("Menù della banca Bushaj\n");
     printf("1.Deposta soldi sul tuo conto\n");
