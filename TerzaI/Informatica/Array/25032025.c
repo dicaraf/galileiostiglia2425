f94eb4c99d01c4a4e8f8a7cb0ae93c86a9ed9507
Dicara Francesco <dicaraf@gmail.com>
2025-03-25 13:17:32 +0000

    creazione vettore ordinato

--- TerzaI/Informatica/Array/25032025.c
+++ TerzaI/Informatica/Array/25032025.c
@@ -1,3 +1,34 @@
 /*riempi vettore ordinato
 ricerca sequenziale
 ricerca dicotomica/binaria*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void shiftDx (int vett[], int dim, int pos){
+    for(int i=dim; i>pos; i--){
+        vett[i]=vett[i-1];
+    }
+}
+int trovaPosizione (int vett[], int dim, int num){
+    int i=0;
+    while(vett[i]<num && i<dim){
+        i++;
+    }
+    return i;
+}
+int main(){
+    int num=0, vett[10], pos=0;
+    srand(time(NULL));
+    printf("inserisci un valore: ");
+        scanf("%d", &vett[0]);
+    for(int i=1; i<10; i++){
+        //printf("inserisci un valore: ");
+        //scanf("%d", &num);
+        num = rand()%7;
+        pos=trovaPosizione(vett, i, num); 
+        shiftDx(vett, i, pos);
+        vett[pos]=num;
+    }
+
+    for(int i=0; i<10; i++) printf("%d - ", vett[i]);
+}
